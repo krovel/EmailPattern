6b9fc79098f2a53b5cf275c14ef5124c5d04a71f
krove <krove@DESKTOP-Q5SED1R>
2020-09-28 21:17:26 +0530

    [Kashif]UC1 added. First mandatory part 'abc' check

--- src/com/capgemini/EmailPattern.java
+++ src/com/capgemini/EmailPattern.java
@@ -1,7 +1,20 @@
 package com.capgemini;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class EmailPattern {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Email pattern check ");
+		System.out.println("Welcome to Email pattern check.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Email for pattern checking: ");
+		String mail = sc.nextLine();
+		Pattern p = Pattern.compile("^abc+");
+		Matcher m = p.matcher(mail);
+		if (m.find()) {
+			System.out.println("Valid Email.");
+		} else {
+			System.out.println("ERROR ! Not a valid Email. Please enter the correct pattern.");
+		}
 	}
 }
